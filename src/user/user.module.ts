b5ff47f85794e810bcd289c2d23ec3e5b95e9174
Pavel Sokolov <49731235+SMPlay@users.noreply.github.com>
2021-10-24 21:37:11 +0300

    Added settings to UserModule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
-@Module({})
+import { UserController } from './user.controller';
+import { User, UserSchema } from './schemas/user.schema';
+import { UserService } from './user.service';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  controllers: [UserController],
+  providers: [UserService],
+})
 export class UserModule {}
