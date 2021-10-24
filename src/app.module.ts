bd86e6509fd3328311e34b54589a00003f8c4888
Pavel Sokolov <49731235+SMPlay@users.noreply.github.com>
2021-10-24 21:12:08 +0300

    Setup mongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+/**
+ * @todo Make login and password as env
+ */
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://Pavel_Sokolov:5gzNWC0xTgAk2wcG@main-cluster.0lcsf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
