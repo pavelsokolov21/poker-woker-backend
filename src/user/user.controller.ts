c86094b8e61e8a51b120526cc0e46db0ba24439b
Pavel Sokolov <49731235+SMPlay@users.noreply.github.com>
2021-10-26 20:29:03 +0300

    Added validation pipe

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,6 +3,7 @@ import { Body, Controller, Post, UseFilters } from '@nestjs/common';
 import { ValidationExceptionFilter } from '../filters/validation-exception.filter';
 import { CreateUserDto } from './dto/createUserDto.dto';
 import { UserService } from './user.service';
+import { ValidationPipe } from '../pipes/validation.pipe';
 
 @Controller('user')
 export class UserController {
@@ -10,7 +11,7 @@ export class UserController {
 
   @Post('/register')
   @UseFilters(new ValidationExceptionFilter())
-  async createUser(@Body() createUserDto: CreateUserDto) {
+  async createUser(@Body(new ValidationPipe()) createUserDto: CreateUserDto) {
     const result = await this.userService.createUser(createUserDto);
 
     return result;
