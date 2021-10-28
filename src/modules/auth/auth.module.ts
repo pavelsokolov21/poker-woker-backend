a050d5ad5d2e455db31255a3058790f931db8712
Pavel Sokolov <49731235+SMPlay@users.noreply.github.com>
2021-10-28 23:40:45 +0300

    Added JWT

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { UserModule } from '../user/user.module';
 import { AuthService } from './auth.service';
@@ -7,7 +9,20 @@ import { AuthController } from './auth.controller';
 import { LocalStrategy } from './local.strategy';
 
 @Module({
-  imports: [UserModule, PassportModule],
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get('JWT_EXPIRES_IN'),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   providers: [AuthService, LocalStrategy],
   controllers: [AuthController],
 })
