06f249a06fa494006a5a95857351b8a705f7bad4
Pavel Sokolov <49731235+SMPlay@users.noreply.github.com>
2021-10-31 15:08:00 +0300

    Added cookie parser

--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -1,19 +1,24 @@
-import { Strategy } from 'passport-jwt';
+import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+
 import { cookieExtractor } from './extractors/cookieExtractor';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      jwtFromRequest: cookieExtractor,
+      jwtFromRequest: ExtractJwt.fromExtractors([cookieExtractor]),
       ignoreExpiration: false,
       secretOrKey: process.env.JWT_ACCESS_TOKEN_SECRET,
     });
   }
 
   async validate(payload: any) {
+    if (!payload) {
+      throw new UnauthorizedException();
+    }
+
     return { id: payload.id, email: payload.email };
   }
 }
