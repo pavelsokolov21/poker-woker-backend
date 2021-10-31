e4fdff90c6278c6ae3473afc6bfb74c72641869a
Pavel Sokolov <49731235+SMPlay@users.noreply.github.com>
2021-10-31 16:43:27 +0300

    Added cookies of tokens

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -1,7 +1,12 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller, Post, Request, Response, UseGuards } from '@nestjs/common';
 
 import { LocalAuthGuard } from '../guards/local-auth.guard';
 import { AuthService } from '../services/auth.service';
+import {
+  COOKIE_ACCESS_TOKEN,
+  COOKIE_REFRESH_TOKEN,
+} from '../../../constants/cookies';
+import { Response as ExpressResponse } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -9,7 +14,19 @@ export class AuthController {
 
   @UseGuards(LocalAuthGuard)
   @Post('/login')
-  async login(@Request() req) {
-    return this.authService.login(req.user);
+  async login(
+    @Request() req,
+    @Response({ passthrough: true }) res: ExpressResponse,
+  ) {
+    const tokens = await this.authService.login(req.user);
+
+    res.cookie(COOKIE_ACCESS_TOKEN, tokens.accessToken, {
+      httpOnly: true,
+    });
+    res.cookie(COOKIE_REFRESH_TOKEN, tokens.refreshToken, {
+      httpOnly: true,
+    });
+
+    return tokens;
   }
 }
